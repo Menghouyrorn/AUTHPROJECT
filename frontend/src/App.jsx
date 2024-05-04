d107b0bc3d348463b2c4f46c2e875ac31b2b34dd
RORN Menghouy <menghouyrorn@gmail.com>
2024-05-04 13:05:22 +0700

    update header component and make private network

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,27 @@
 
-import { BrowserRouter,Routes,Route } from "react-router-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Home from "./pages/Home"
 import About from "./pages/About"
 import Signin from "./pages/Signin"
 import SignUp from "./pages/SignUp"
 import Profile from "./pages/Profile"
 import Navbar from "./components/Navbar"
+import PrivateRoute from "./components/PrivateRoute"
 
 export default function App() {
   return (
     <div>
       <BrowserRouter>
-        <Navbar/>
+        <Navbar />
         <Routes>
-          <Route path="/" element={<Home/>}/>
-          <Route path="/about" element={<About/>}/>
-          <Route path="/sign-in" element={<Signin/>}/>
-          <Route path="/sign-up" element={<SignUp/>}/>
-          <Route path="/profile" element={<Profile/>}/>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/sign-in" element={<Signin />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route element={<PrivateRoute />}>
+            <Route path="/profile" element={<Profile />} />
+          </Route>
+
         </Routes>
       </BrowserRouter>
     </div>
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+
 
 const Navbar = () => {
+
+    const { currentUser } = useSelector((state) => state.user);
+
     return (
         <div className='bg-slate-200'>
             <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
@@ -16,7 +21,9 @@ const Navbar = () => {
                         <li>About</li>
                     </Link>
 
-                    <Link to={'/sign-in'}><li>Sign In</li></Link>
+                    <Link to={'/profile'}>
+                        {currentUser ? (<img src={currentUser.profilePicture} className='h-7 w-7 rounded-full object-cover' alt='profile' />) : (<li>Sign In</li>)}
+                    </Link>
                 </ul>
             </div>
         </div>
